0280706c475b67a619977f967c1891fce2b0a424
Xintong Zhou <zxt@bcsner.com>
2019-04-20 07:31:05 +0800

    Add ldap-contacts-suggestions plugin

--- rootfs/usr/local/bin/run.sh
+++ rootfs/usr/local/bin/run.sh
@@ -8,6 +8,10 @@ sed -i "s/<MEMORY_LIMIT>/$MEMORY_LIMIT/g" /etc/php7/php-fpm.conf
 if [ -d "/rainloop/data/_data_/_default_/plugins/postfixadmin-change-password" ]; then
   rm -rf /rainloop/data/_data_/_default_/plugins/postfixadmin-change-password
 fi
+# Remove ldap-contacts-suggestions plugin if exist
+if [ -d "/rainloop/data/_data_/_default_/plugins/ldap-contacts-suggestions" ]; then
+  rm -rf /rainloop/data/_data_/_default_/plugins/ldap-contacts-suggestions
+fi
 
 # Set log output to STDOUT if wanted (LOG_TO_STDOUT=true)
 if [ "$LOG_TO_STDOUT" = true ]; then
@@ -17,9 +21,11 @@ if [ "$LOG_TO_STDOUT" = true ]; then
   sed -i "s/.*error_log.*$/error_log = \/dev\/stdout/" /etc/php7/php-fpm.conf
 fi
 
-# Add postfixadmin-change-password plugin
 mkdir -p /rainloop/data/_data_/_default_/plugins/
+# Add postfixadmin-change-password plugin
 cp -r /usr/local/include/postfixadmin-change-password /rainloop/data/_data_/_default_/plugins/
+# Add ldap-contacts-suggestions plugin
+cp -r /usr/local/include/ldap-contacts-suggestions /rainloop/data/_data_/_default_/plugins/
 
 # Fix permissions
 chown -R $UID:$GID /rainloop/data /services /var/log /var/lib/nginx
